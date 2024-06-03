e8583fd8b8b24082665afd38d60695d93256f972
Clarita254 <clara.waithaka@strathmore.edu>
2024-06-03 13:03:39 +0300

    Created a javascript folder

---
+++ javascript/script.js
@@ -0,0 +1,16 @@
+function toggleFAQ(element){
+    const faqItem=element.parentElement;
+    const faqAnswer=faqItem.querySelector('.faq-answer');
+    const faqIcon=faqItem.querySelector('.faq-icon');
+
+    if(faqAnwer.style.display=='block'){
+        faqAnswer.style.display='none';
+        faqIcon.textContent='+';
+        faqItem.classList.remove('active');
+
+    }else{
+        faqAnswer.style.display = 'block';
+        faqIcon.textContent= '-';
+        faqItem.classList.add('active');
+    }
+}
