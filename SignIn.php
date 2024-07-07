de52e3d65ed1caec1cbebec8252dbca63695dd8c
Clarita254 <clara.waithaka@strathmore.edu>
2024-07-07 09:49:59 +0300

    Changed code of signUp page

--- SignIn.php
+++ SignIn.php
@@ -1,71 +1,58 @@
 
-<?php require_once("includes/db_connect.php");?>
-<?php include_once("templates/header.php");?>
-    <link rel="stylesheet" href="https:/cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-
+<?php require_once("includes/db_connect.php");
+ include_once("templates/header.php");?>
+  <?php include_once("templates/nav.php");?>  
     <style>
-        .avatar{
-            vertical-align:middle;
-            width:50px;
-            height:50px;
-            border-radius:50%;
+
+        body{
+background-image: url(Images/Background/bb2.jpg);
+        }
+
+        .header-h{
+            color:lightseagreen;
+            font-size:large;
         }
     </style>
 
 <body>
-
-    <?php include_once("templates/nav.php");?>
-
-
-
-
-    <!---Button to open the modal login form-->
-    <button onclick="document.getElementById('id01').style.display='block'">
- Login</button>
- <!--The Modal-->
- <div id="id01"class="modal">
-    <span onclick="document.getElementById('id01').style.display='none'" class="close"title="Close Modal">&times;</span>
-
-
-    <!--Modal Content-->
-<form class="modal-content animate"action="/action_page.php" action="user_processes.php"method="POST" autocomplete="off">
-    <div class="imgcontainer">
-        <img src="Images/People/img_avatar6.png"alt="Avatar" class="avatar">
-        <img src="Images/People/img_avatar2.png"alt="Avatar" class="avatar">
-
-    </div>
-    <div class="containerSignIn">
-        
-        <label for="Username"><b>Username</b></label>
-        <input type="text"placeholder="Enter Username"name="Username" required>
-
-        <label for="password"><b>Password</b></label>
-        <input type="password" placeholder="EnterPassword"name="password"required>
-        <button type="submit">Login</button>
-        <label>
-           <input type="checkbox"checked="checked"name="remember">Remember <menu></menu>
-        </label>
-
-    </div>
-    <div class="container"style="background-color:#f1f1f1">
-        <button type="button" onclick="document.getElementById('id01').style.display='none'"class="cancelbutton">Cancel</button>
-        <span class="password">Forgot <a href="#">Password</a></span>
-
-    </div>
-</form>
- </div>
-
- <script>
-    // Get the modal
-    var modal = document.getElementById('id01');
+<div class= "header-h">
+    <h1>SignIn</h1>
+</div>
+       
     
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    }
-    </script>
+<div class="SignIn">
+<form action=""method="">
+
+<div class="imgcontainer">
+<img src="Images/People/img_avatar2.png"alt="Avatar"class="avatar">
+<img src="Images/People/img_avatar6.png"alt="Avatar"class="avatar">
+</div>
+
+<div class="container">
+<label for="username"><b>Username:</label>
+<input type="text"placeholder="Enter Username" name="username"required><br>
+
+<label for="password"><b>Password:</label>
+<input type="password"placeholder="Enter Password"name="password"required>
+<label>
+    <input type="checkbox"checked="checked"name="remember">Remember me
+</label>
+
+<button type="submit">Login</button>
+
+<button type="button" class="cancelbutton">Cancel</button>
+    <span class="password"><a href="#">Forgot Password?</a></span>
 
 
 
+    </div>
+
+ 
+</form>
+</div>
+
+<div class="container">
+<?php require_once("templates/footer.php");?>
+<div>
+
+ 
--- SignUp.php
+++ SignUp.php
@@ -1,22 +1,22 @@
-<?php require_once("includes/db_connect.php");
-     include_once("templates/header.php");
-     include_once("templates/nav.php");
-
+<?php 
+require_once("includes/db_connect.php");
+include_once("templates/header.php");
+include_once("templates/nav.php");
 
 if(isset($_POST["SignUp"])){
+  $fullname = mysqli_real_escape_string($conn, $_POST["fullname"]);
+  $username = mysqli_real_escape_string($conn, $_POST["username"]);
+  $emailAddress = mysqli_real_escape_string($conn, $_POST["emailAddress"]);
+  $password = mysqli_real_escape_string($conn, $_POST["password"]);
+  $role = mysqli_real_escape_string($conn, $_POST["roleId"]);
+  $gender = mysqli_real_escape_string($conn, $_POST["genderId"]);
+  $confirmpassword = mysqli_real_escape_string($conn, $_POST["confirmpassword"]);
 
-  $fullname=mysqli_real_escape_string($conn,$POST["fullname"]);
-  $username=mysqli_real_escape_string($conn,$POST["username"]);
-  $emailAddress=mysqli_real_escape_string($conn,$POST["emailAddress"]);
-  $gender=mysqli_real_escape_string($conn,$POST["genderId"]);
-  $role=mysqli_real_escape_string($conn,$POST["roleId"]);
-  $passphrase=mysqli_real_escape_string($conn,$POST["passphrase"]);
-
-//Verify email format
-if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
-header("Location:?wrong_email_format");
-exit();
-}
+  // Verify email format
+  if(!filter_var($emailAddress, FILTER_VALIDATE_EMAIL)){
+    header("Location:?wrong_email_format");
+    exit();
+  }
 
 //verify authorized email domain(gmail.com)
 //verify password and repeat password are identical
@@ -25,102 +25,93 @@ exit();
 //verify the new username does not exist already in the database
 //verify that the fullname contains only letters and a space and aquotation symbol
 
-
 }
 ?>
+  <style>
+    h1 {
+      text-align: center;
+      margin-bottom: 20px;
+      color: white;
+    }
+    p {
+      display: block;
+      margin-bottom: 5px;
+      font-size:large;
+      color: black;
+    }
 
-
-<style>
-h1{
-text-align:centre;
-margin-bottom:20px;
-color:white;
-}
-
-p{
-display:block;
-margin-bottom:5px;
-font-size: x-large;
-color:black;
-
-}
-
-    </style>
-<body>
-
-<div class="header">
-<h1> Sign Up </h1> 
-</div>
-
-<div class="containerSignUp">
-<form action="action_page.php">
+    p1{
+      font-size:large;
+      font-weight:bold;
+    }
     
-<p>Please fill in this form to create an account </p>
-   
-<hr>
+  </style>
+<body>
+  <div class="header">
+    <h1> Sign Up </h1> 
+  </div>
 
-<label for ="fullname"><b>Fullname:</b></label>
-<input type="text" name="fullname"id="fullname"maxlength="50"placeholder="Fullname" required><br>
+  <div class="containerSignUp">
+    <form action="" method="POST">
+      <p1>Please fill in this form to create an account </p1>
+      <hr>
+      <label for="fullname"><b>Fullname:</b></label>
+      <input type="text" name="fullname" id="fullname" maxlength="50" placeholder="Fullname" required><br>
 
-<label for ="Username"><b>Username:</b></label>
-<input type="text" name="Username" maxlength="10" id="username"placeholder="Username" required><br>
+      <label for="username"><b>Username:</b></label>
+      <input type="text" name="username" maxlength="10" id="username" placeholder="Username" required><br>
 
-<label for ="email"><b>EmailAddress:</b></label>
-<input type="email"name="email"maxlength="20"id="email"placeholder="Email Address"required><br>
+      <label for="email"><b>EmailAddress:</b></label>
+      <input type="email" name="emailAddress" maxlength="50" id="emailAddress" placeholder="Email Address" required><br>
+      <?php if(isset($_GET["wrong_email_format"])){print "<span class='error_form'>Wrong email format</span>";}?>
 
-<?php if(isset($_GET["wrong_email_format"])){print "<span class='error_form'>wrong email format</span>";}?>
+      <label for="genderId">Gender:</label>
+      <select name="genderId" id="genderId" required>
+        <option value="">---Select Gender---</option>
+        <?php
+        $sel_gen = "SELECT * FROM `gender` ORDER BY gender ASC";
+        $sel_gen_res = $conn->query($sel_gen);
+        while($sel_gen_row = $sel_gen_res->fetch_assoc()){
+        ?>
+        <option value="<?php print $sel_gen_row["genderId"];?>"><?php print $sel_gen_row["gender"];?></option>
+        <?php }?>
+      </select>
+      <br>
 
-<label for=genderId>Gender:</label>
-<select name=genderId id="genderId"required>
-  <option value="">---Select Gender--</option>
-<?php
-  $sel_gen="SELECT* FROM`gender`ORDER BY gender ASC";
-  $Sel_gen_res=$conn->query($sel_gen);
-  while($sel_gen_row = $sel_gen_res->fetch_assoc()){
-?>
-<option value="<?php print $sel_gen_row["genderId"];?>"><?php print $sel_gen_row["gender"];?></option>
-<?php }?>
+      <label for="roleId">Role:</label>
+      <select name="roleId" id="roleId" required>
+        <option value="">---Select Role---</option>
+        <?php
+        $sel_role = "SELECT * FROM `role` ORDER BY role ASC";
+        $sel_role_res = $conn->query($sel_role);
+        while($sel_role_row = $sel_role_res->fetch_assoc()){
+        ?>
+        <option value="<?php print $sel_role_row["roleId"];?>"><?php print $sel_role_row["role"];?></option>
+        <?php }?>
+      </select>
+      <br>
 
-</select>
-<br>
+      <label for="password"><b>Password:</b></label>
+      <input type="password" name="password" id="password" maxlength="16" placeholder="Password" required><br>
 
-<label for=roleId>Role:</label>
-<select name=roleId id="roleId"required>
-  <option value="">--Select Role-</option><br>
-<?php
-  $sel_role="SELECT*FROM`role`ORDER BY role ASC";
-  $Sel_role_res=$conn->query($sel_role);
-  while($sel_role_row = $sel_role_res->fetch_assoc()){
-?>
-<option value="<?php print $sel_gen_row["roleId"];?>"><?php print $sel_gen_row["role"];?></option>
-<?php
-  }?>
-</select>
-<br>
-<!----->
-<label for="password"><b>Password:</b></label>
-<input type="password"name="passphrase"id="password"placeholder="Password" required><br>
+      <label for="confirmpassword"><b>Confirm Password:</b></label>
+      <input type="password" name="confirmpassword" id="confirmpassword" maxlength="16" placeholder="Confirm Password" required><br>
 
+      <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
+      <br>
+      <p>By creating an account you agree to our <a href="SignUp.php" style="color:dodgerblue;"> Terms and Privacy</a></p>
 
-<label for ="Confirm-password"><b>Confirm Password:</b></label>
-<input type="password"name="confirm-password"id="confirm password"placeholder="Confirm Password"required><br>
+      <div class="clearfix">
+        <button type="button" class="cancelbutton">Cancel</button>
+        <button type="submit" name="SignUp" class="signupbutton">Sign Up</button>
+      </div>
+    </form>
+  </div>
 
-
-<input type="checkbox"checked="checked"name="remember"style="margin-bottom:15px">Remember me </label><br>
-<p1>By creating an account you agree to our <a href="SignUp.php" style="color:dodgerblue"> Terms and Privacy</a></p1>
-
-<div class="clearfix">
-    <button type="button" class="cancelbutton">Cancel</button>
-    <button type="submit" class="signUpbutton">SignUp</button>
- </div>
- 
- </form>
- </div>
-
- <div class="account-info">
-<p class="note">Already have an account?<a href="SignIn.php">SignIn</a></p>
-</div>
+  <div class="account-info">
+    <p class="note">Already have an account? <a href="SignIn.php">Sign In</a></p>
+  </div>
 </body>
+</html>
 
 <?php require_once("templates/footer.php");?>
-
