89733bb862c92008972da49ac9e0dbdc908d2a09
Clarita254 <clara.waithaka@strathmore.edu>
2024-07-08 12:47:35 +0300

    Added a feature to re-direct user to Home page if they logged in successfully

--- SignIn.php
+++ SignIn.php
@@ -4,52 +4,48 @@ require_once("includes/db_connect.php");
 include_once("templates/header.php");
 include_once("templates/nav.php");
 
-//SignIn process
-if(isset($_POST["signIn"])){
 
-    //Variable declaration
-    $entered_UserName=mysqli_real_escape_string($conn,addslashes($_POST["username"]));
-    $entered_Password=mysqli_real_escape_string($conn,addslashes($_POST["password"]));
- 
-    //Verify is the entered username matches any record
-    $spot_username="SELECT*FROM users WHERE Username ='$entered_Username'LIMIT 1";
- 
- 
-    //Executing the select query
-    $userName_res=$conn->query($spot_username);
- 
-    //Count at least one matching row
-    if($userName_res->num_rows>0){
-       //Create a session
-       $_SESSION["control"]=$userName_res->fetch_assoc();
- 
-       //Use the session to fetch the stored password
-       $stored_password=$_SESSION["control"]["password"];
- 
-       //Verify if the entered_password is identical to the stored_password
-       if(password_verify($entered_password,$stored_password)){
-          //If the two passwords match,red-direct to index.php
-          header("Location:index.php");
-          exit();
-       }
-       else{
-          //Otherwise destroy the control session and re-direct back to SignIn.php
-          unset($_SESSION["control"]);
-          header("Location:SignIn.php");
-          exit();
-          
-         
-       }
-    } else{
- //Otherwise re-direct back to SignIn.php
-       header("Location:SignIn.php");
-      exit();
-       }
- 
+
+// SignIn process
+if (isset($_POST["signIn"])) {
+
+    // Variable declaration
+    $entered_UserName = mysqli_real_escape_string($conn, $_POST["username"]);
+    $entered_Password = mysqli_real_escape_string($conn, $_POST["password"]);
+
+    // Verify if the entered username matches any record
+    $spot_username = "SELECT * FROM users WHERE Username = '$entered_UserName' LIMIT 1";
+
+    // Executing the select query
+    $userName_res = $conn->query($spot_username);
+
+    // Check if at least one matching row is found
+    if ($userName_res->num_rows > 0) {
+        // Fetch the user record
+        $user_record = $userName_res->fetch_assoc();
+
+        // Use the fetched record to get the stored password
+        $stored_password = $user_record["password"];
+
+        // Verify if the entered password is identical to the stored password
+        if (password_verify($entered_Password, $stored_password)) {
+            // If the passwords match, create a session
+            $_SESSION["user"] = $user_record;
+
+            // Redirect to index.php
+            header("Location:index.php");
+            exit();
+        } else {
+            // Otherwise, display an error message for incorrect password
+            $login_error = "Incorrect password. Please try again.";
+        }
+    } else {
+        // Otherwise, display an error message for username not found
+        $login_error = "Username not found. Please try again.";
     }
-  
-   
+}
 ?>
+
 <style>
     body {
         background-image: url(Images/Background/bb2.jpg);
@@ -58,9 +54,12 @@ if(isset($_POST["signIn"])){
         color: lightseagreen;
         font-size: large;
     }
-    .Sign,p{
-font-size:medium;
-font-weight:bold;
+    .Sign, p {
+        font-size: medium;
+        font-weight: bold;
+    }
+    .message {
+        color: red;
     }
 </style>
 
@@ -81,8 +80,8 @@ font-weight:bold;
             </div>
 
             <div class="container">
-                <label for="emailAddress"><b>Email Address:</b></label>
-                <input type="text" placeholder="Enter Email Address" name="emailAddress" required><br>
+                <label for="username"><b>Username:</b></label>
+                <input type="text" placeholder="Enter Username" name="username" required><br>
 
                 <label for="password"><b>Password:</b></label>
                 <input type="password" placeholder="Enter Password" name="password" required><br>
@@ -91,21 +90,16 @@ font-weight:bold;
                     <input type="checkbox" checked="checked" name="remember"> Remember me
                 </label>
 
-                <button type="submit">Login</button>
+                <button type="submit" name="signIn">Login</button>
                 <button type="button" class="cancelbutton">Cancel</button>
                 <span class="password"><a href="#">Forgot Password?</a></span>
             </div>
         </form>
         <p class="account-info">Don't have an account? <a href="SignUp.php">Sign Up</a></p>
-        <p class="Sign">Thank you for logging In!</p>
-        </div>
-
+        
     </div>
 
-    
-
-
     <div class="container">
-        <?php require_once("templates/footer.php");?>
+        <?php require_once("templates/footer.php"); ?>
     </div>
 </body>
